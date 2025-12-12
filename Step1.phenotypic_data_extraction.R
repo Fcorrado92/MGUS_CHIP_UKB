214f678e3bdf069bae60bc27142a8e0cf792deda
Fcorrado92 <francescocorrado2@gmail.com>
2025-12-12 02:40:59 +0000

    step1

--- Step1.phenotypic_data_extraction.R
+++ Step1.phenotypic_data_extraction.R
@@ -78,3 +78,58 @@ overlap_ids <- intersect(mgus_ids, mm_ids)
 mgus_ids
 mm_ids
 overlap_ids
+
+
+
+# -------------------------------------------------------------------------
+#Load data from self-reported, primary-care, hospital admission
+# -------------------------------------------------------------------------
+data<-read_csv("/mnt/project/extract_fields_ttyd/Other_sources_data.csv")
+nrow(data)
+colnames(data)
+
+# MGUS ICD10 code
+mgus_code <- "D47.2"
+
+# TRUE if the string contains D47.2 as a full code
+has_mgus <- grepl("\\bD47\\.2\\b", data$`Diagnoses - ICD10`)
+
+# Replace NA with FALSE
+has_mgus[is.na(has_mgus)] <- FALSE
+
+# How many MGUS
+sum(has_mgus)
+
+# If you have an ID column (e.g. eid), get their IDs
+mgus_ids <- data$`Participant ID`[has_mgus]
+
+# Subset full rows
+mgus_data <- data[has_mgus, ]
+
+
+# MM ICD10 code
+mm_icd10 <- "C90.0"
+
+# Flag rows where the ICD10 string contains MM (C90.0)
+has_mm <- grepl("\\bC90\\.0\\b", data$`Diagnoses - ICD10`)
+
+# Replace NA with FALSE
+has_mm[is.na(has_mm)] <- FALSE
+
+# Number of MM cases
+sum(has_mm)
+
+# Extract Participant IDs
+mm_ids <- data$`Participant ID`[has_mm]
+
+# Extract full MM rows
+mm_data <- data[has_mm, ]
+
+
+mgus_ids <- unique(data$`Participant ID`[has_mgus])
+mm_ids <- unique(data$`Participant ID`[has_mm])
+overlap_ids <- intersect(mgus_ids, mm_ids)
+
+mgus_ids
+mm_ids
+overlap_ids
