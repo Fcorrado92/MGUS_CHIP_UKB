d6477ec1699476c11bac03195330ffd674618ea7
Fcorrado92 <francescocorrado2@gmail.com>
2026-01-07 02:30:15 +0000

    .

--- Step1.phenotypic_data_extraction_GP.R
+++ Step1.phenotypic_data_extraction_GP.R
@@ -6,11 +6,17 @@ install.packages("cmprsk")
 install.packages("tidycmprsk")
 
 library(readr)
+library(dplyr)
 
 # -------------------------------------------------------------------------
 #extract codes from primary care data
 # -------------------------------------------------------------------------
 data<-read_csv("/mnt/project/extract_fields_ttyd/GP_clinical.csv")
+
+nrow(data)
+# 118154716
+
+#each row is an encounter
 
 #MGUS codes CTV3 and R2
 mgus_ctv3<-c("C331.","C332.","C332z","Xa0l6","Xa0SJ","Xa36d","Xa36e","Xa36h","Xa36k","Xa36m","XaIt4","XE11b")
@@ -21,6 +27,21 @@ mgus_readv2<-c("BBm7.",               "C3322",               "C3310",           
 mgus_data_v3<-data%>%dplyr::filter(`CTV3 (Read v3) code`%in%mgus_ctv3)
 mgus_data_v2<-data%>%dplyr::filter(`Read v2 code`%in%mgus_readv2)
 mgus_data<-rbind(mgus_data_v3, mgus_data_v2)
+
+
+#how many patients and how many patients have multiple encounters?
+mgus_data%>%summarise(n=n_distinct(`Participant ID`))
+# 521
+
+encounter_n<-mgus_data%>%group_by(`Participant ID`)%>%summarise(n=n_distinct(`Date clinical code was entered`))
+ggplot(encounter_n, aes(x=n))+geom_histogram()
+# table(encounter_n$n)
+# 
+# 1   2   3   4   5   6   7  10 
+# 385  87  28   6   9   4   1   1 
+
+#MGUS code is mostly inserted only once
+
 
 #Select only cols ID and Date and then keep only first date
 mgus_data_filt<-mgus_data%>%select(`Participant ID`,`Date clinical code was entered`, `Data provider`)
@@ -70,6 +91,17 @@ mm_readv2<-c("BBn2.","BBr30" ,"BBr3z", "BBr3.","BBn0.",
 mm_data_v3<-data%>%dplyr::filter(`CTV3 (Read v3) code`%in%mm_ctv3)
 mm_data_v2<-data%>%dplyr::filter(`Read v2 code`%in%mm_readv2)
 mm_data<-rbind(mm_data_v3, mm_data_v2)
+
+#how many patients and how many patients have multiple encounters?
+mm_data%>%summarise(n=n_distinct(`Participant ID`))
+# 322
+
+encounter_n_mm<-mm_data%>%group_by(`Participant ID`)%>%summarise(n=n_distinct(`Date clinical code was entered`))
+ggplot(encounter_n_mm, aes(x=n))+geom_histogram()
+ table(encounter_n_mm$n)
+
+ # 1   2   3   4   5   6   7   8   9  10  12  14  16  26  27  28  34  36  44  67 
+ # 182  64  26  18   7   3   1   3   2   3   4   1   1   1   1   1   1   1   1   1  
 
 #Select only cols ID and Date and then keep only first date
 mm_data_filt<-mm_data%>%select(`Participant ID`,`Date clinical code was entered`)
